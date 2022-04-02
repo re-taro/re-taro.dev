90612172874aa26f0f5a30f239877051a716016d
Re-taro <aminokacho@gmail.com>
2022-04-02 23:43:07 +0900

    ✨ update next.js config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,77 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
+/**
+ * @docs
+ * - https://scotthelme.co.uk/content-security-policy-an-introduction/
+ * - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+ */
+const ContentSecurityPolicy = `
+  default-src 'self';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/;
+  media-src 'none';
+  connect-src *;
+  font-src 'self' https://fonts.gstatic.com/;
+`
+
+/**
+ * @docs
+ * - https://nextjs.org/docs/advanced-features/security-headers
+ */
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, '')
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'geolocation=()'
+  }
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  headers: async () => {
+    return [
+      {
+        source: '/',
+        headers: securityHeaders
+      },
+      {
+        source: '/:path*',
+        headers: securityHeaders
+      }
+    ]
+  },
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
@@ -14,6 +81,9 @@ const nextConfig = {
       })
     }
     return config
+  },
+  eslint: {
+    dirs: ['src']
   }
 }
 
