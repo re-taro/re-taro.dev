0965546fe801fb36deac1ea0be11248c68782762
Re-taro <aminokacho@gmail.com>
2022-04-02 22:49:23 +0900

    ✨ add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { GlobalStyles } from 'twin.macro'
 
-const App: NextPage<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />
+const App: NextPage<AppProps> = ({ Component, pageProps }) => (
+  <React.Fragment>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </React.Fragment>
+)
 
 export default App
