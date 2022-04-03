8ff62e8e29fc9b9d8f30c44128390460b313407a
Re-taro <aminokacho@gmail.com>
2022-04-03 11:40:48 +0900

    🔍️ update SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from 'next'
+import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 import React from 'react'
 import { GlobalStyles } from 'twin.macro'
+import { defaultSeoConfig } from '../utils/next-seo'
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => (
   <React.Fragment>
+    <DefaultSeo {...defaultSeoConfig} />
     <GlobalStyles />
     <Component {...pageProps} />
   </React.Fragment>
