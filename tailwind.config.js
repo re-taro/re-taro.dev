f362065c135f0a36cdb767885756e921b2f4047d
Re-taro <aminokacho@gmail.com>
2022-04-03 17:22:18 +0900

    🎨 add my own color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,45 @@
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig} */
 module.exports = {
-  darkMode: 'class',
-  plugins: [],
   content: ['./src/**/**/**/*.{ts,tsx}'],
-  theme: {},
+  darkMode: 'class',
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio')
+  ],
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        aurora: {
+          green: '#A3BE8C',
+          orange: '#D08770',
+          purple: '#B48EAD',
+          red: '#BF616A',
+          yellow: '#EBCB8B'
+        },
+        frost: {
+          100: '#8FBCBB',
+          200: '#88C0D0',
+          300: '#81A1C1',
+          400: '#5E81AC'
+        },
+        night: {
+          100: '#4C566A',
+          200: '#434C5E',
+          300: '#3B4252',
+          400: '#2E3440'
+        },
+        snow: {
+          100: '#ECEFF4',
+          200: '#E5E9F0',
+          300: '#D8DEE9'
+        }
+      }
+    }
+  },
   variants: {
     extend: {}
   }
