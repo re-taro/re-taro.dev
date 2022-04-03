5bf2e9875ace37bd76eea9f0fec37e52801d555f
Re-taro <aminokacho@gmail.com>
2022-04-03 20:53:31 +0900

    🚑️ hotfix

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,16 +1,19 @@
-export const ci = {
-  assert: {
-    assertions: {
-      'categories:accessibility': ['error', { minScore: 0.9 }],
-      'categories:best-practices': ['error', { minScore: 0.9 }],
-      'categories:performance': ['error', { minScore: 0.9 }],
-      'categories:seo': ['error', { minScore: 0.9 }]
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  ci: {
+    assert: {
+      assertions: {
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }]
+      }
+    },
+    collect: {
+      staticDistDir: './out/'
+    },
+    upload: {
+      target: 'temporary-public-storage'
     }
-  },
-  collect: {
-    staticDistDir: './out/'
-  },
-  upload: {
-    target: 'temporary-public-storage'
   }
 }
