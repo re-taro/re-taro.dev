29f08389eaa5de86082b9aae378508193c6ef899
Re-taro <aminokacho@gmail.com>
2022-04-21 13:08:21 +0900

    ✨ update emoji
    use Twemoji parser

--- src/components/atoms/twemojii/index.tsx
+++ src/components/atoms/twemojii/index.tsx
@@ -1,16 +1,19 @@
-import Image from 'next/image'
 import React from 'react'
+import twemoji from 'twemoji'
 
 type TwemojiProperties = {
   emoji: string
 }
 
-const EMOJI_URL = process.env.EMOJI_URL || 'http://localhost:3023'
-
 const Twemoji: React.VFC<TwemojiProperties> = ({ emoji }) => (
-  <span>
-    <Image width={150} height={150} src={`${EMOJI_URL}/${emoji}.svg`} alt={emoji} draggable={false} />
-  </span>
+  <span
+    dangerouslySetInnerHTML={{
+      __html: twemoji.parse(emoji, {
+        ext: '.svg',
+        folder: 'svg'
+      })
+    }}
+  />
 )
 
 export default React.memo(Twemoji)
