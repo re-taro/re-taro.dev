67a16ee681e4738478bcc7794dccc9cd0e54a57a
Re-taro <aminokacho@gmail.com>
2022-04-21 16:40:15 +0900

    ✨ fix icon

--- src/components/molecules/toggle-button/index.tsx
+++ src/components/molecules/toggle-button/index.tsx
@@ -12,7 +12,7 @@ const ToggleButton: React.VFC = () => {
       as={'button'}
       aria-label={'Toggle theme'}
       variant={'icon'}
-      leftIcon={mounted && theme === 'dark' ? 'ri:moon-fill' : 'ri:sun-fill'}
+      leftIcon={mounted && theme === 'dark' ? 'ri:sun-fill' : 'ri:moon-fill'}
       iconStyles={tw`bg-snow-300 dark:bg-night-300 text-2xl hover:bg-snow-200 dark:hover:bg-night-200`}
       boxStyles={tw`p-1 hover:outline-none hover:ring-2 hover:ring-frost-100 w-8 h-8 mx-1`}
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
