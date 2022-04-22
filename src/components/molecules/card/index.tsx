c0477141cc215b994d2aeb842ed5810eef1b3c68
Re-taro <aminokacho@gmail.com>
2022-04-23 02:23:40 +0900

    ✨ fix work card

--- src/components/molecules/card/index.tsx
+++ src/components/molecules/card/index.tsx
@@ -3,12 +3,11 @@ import Link from 'next/link'
 import React from 'react'
 import tw from 'twin.macro'
 import type { HomeQuery } from '../../../graphql'
-import { Flex } from '../../atoms/flex'
 import { Text } from '../../atoms/text'
 
-const CardBox = tw.div`border-2 border-night-100 dark:border-night-400 shadow-card rounded-[5rem] cursor-pointer items-center overflow-hidden justify-center relative transition delay-150 ease-in-out hover:-translate-y-2 hover:scale-[1.02] hover:shadow-xl`
+const CardBox = tw.div`border-2 border-night-100 dark:border-night-400 shadow-card rounded-[20px] cursor-pointer items-center overflow-hidden justify-center relative transition delay-150 ease-in-out hover:-translate-y-2 hover:scale-[1.02] hover:shadow-xl`
 
-const Layer = tw.div`absolute justify-center bottom-0 p-0 h-full w-full z-20 bg-gradient-to-t from-[rgba(45, 55, 72, 0.6)] via-[rgba(203, 213, 224, 0.4)] to-[rgba(237, 242, 247, 0)] dark:from-[rgba(0, 0, 0, 0.5)] dark:to-[rgba(237, 242, 247, 0)] transition delay-150 ease-in-out`
+const Layer = tw.div`flex flex-col absolute justify-end bottom-0 p-6 h-full w-full z-20 bg-gradient-to-t from-[rgba(45, 55, 72, 0.6)] via-[rgba(203, 213, 224, 0.4)] to-[rgba(237, 242, 247, 0)] dark:from-[rgba(0, 0, 0, 0.5)] dark:to-[rgba(237, 242, 247, 0)]`
 
 type CardProperties = React.ComponentProps<React.ReactHTML['div']> & {
   projectData: HomeQuery['works'] | undefined
@@ -23,7 +22,7 @@ const Card: React.VFC<CardProperties> = ({ projectData, ...rest }) => (
             <Image
               src={project.imageUrl}
               height={200}
-              width={200}
+              width={355}
               layout={'responsive'}
               objectFit={'cover'}
               objectPosition={'50% 0'}
@@ -31,9 +30,9 @@ const Card: React.VFC<CardProperties> = ({ projectData, ...rest }) => (
             />
           </div>
           <Layer>
-            <Flex css={tw`flex-row items-center`}>
-              <Text css={tw`font-bold text-lg text-white`}>{project.title}</Text>
-            </Flex>
+            <div css={tw`flex flex-row items-center`}>
+              <Text as={'p'} css={tw`font-bold text-lg text-white`}>{project.title}</Text>
+            </div>
           </Layer>
         </CardBox>
       </Link>
