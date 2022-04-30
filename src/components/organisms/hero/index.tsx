b7dbe324b4462bfe90a39d9d68cd57f2fca2ad00
Re-taro <aminokacho@gmail.com>
2022-04-30 16:29:40 +0900

    🏷️ remove unuse type

--- src/components/organisms/hero/index.tsx
+++ src/components/organisms/hero/index.tsx
@@ -8,11 +8,6 @@ const HeroWrapper = tw.div`h-[90vh] items-center grid`
 
 const HeroBox = tw.div`align-middle w-full sm:w-[80%] md:w-[60%] sm:mt-0 sm:self-center`
 
-export type Hero = {
-  primary: string
-  position: string
-}
-
 type HeroSectionProperties = React.ComponentProps<React.ReactHTML['div']> & {
   data: HomeQuery['basic'] | undefined
 }
