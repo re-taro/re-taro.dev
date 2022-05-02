2986bfa56f37b1eda5b762662c2bd68a7b75da82
Re-taro <aminokacho@gmail.com>
2022-05-02 18:26:08 +0900

    ✨ prefetch the endpoint

--- src/components/templates/posts/index.tsx
+++ src/components/templates/posts/index.tsx
@@ -1,5 +1,6 @@
 import { NextSeo } from 'next-seo'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 import tw from 'twin.macro'
 import { Grid } from '~/components/atoms/grid'
 import { Heading } from '~/components/atoms/heading'
@@ -14,29 +15,41 @@ type PostsPropeties = {
   data: PostsQuery | undefined
 }
 
-const Posts: React.FC<PostsPropeties> = ({ data }) => (
-  <React.Fragment>
-    <NextSeo
-      title={'Blog Posts'}
-      canonical={'https://re-taro.dev/posts'}
-      openGraph={{
-        images: [{
-          alt: 'Posts | re-taro.dev ogp',
-          url: GenOgp('Posts | re-taro')
-        }],
-        title: 'Blog Posts | re-taro'
-      }}
-    />
-    <PostsHead>
-      <Heading as={'h5'} css={tw`mb-2`}>Blog Posts</Heading>
-      <Text as={'p'} css={tw`text-lg`}>Just some writings</Text>
-    </PostsHead>
-    <Grid css={tw`gap-16 my-12`}>
-      {data?.posts.reverse().map((post, key) => (
-        <PostCard post={post} key={key} />
-      ))}
-    </Grid>
-  </React.Fragment>
-)
+const Posts: React.FC<PostsPropeties> = ({ data }) => {
+  const router = useRouter()
+  useEffect(() => {
+    data?.posts.reverse().map(post => router.prefetch('/posts/[id]', `/posts/${post.id}`))
+  }, [data])
+  return (
+    <React.Fragment>
+      <NextSeo
+        title={'Blog Posts'}
+        canonical={'https://re-taro.dev/posts'}
+        openGraph={{
+          images: [
+            {
+              alt: 'Posts | re-taro.dev ogp',
+              url: GenOgp('Posts | re-taro')
+            }
+          ],
+          title: 'Blog Posts | re-taro'
+        }}
+      />
+      <PostsHead>
+        <Heading as={'h5'} css={tw`mb-2`}>
+          Blog Posts
+        </Heading>
+        <Text as={'p'} css={tw`text-lg`}>
+          Just some writings
+        </Text>
+      </PostsHead>
+      <Grid css={tw`gap-16 my-12`}>
+        {data?.posts.reverse().map((post, key) => (
+          <PostCard post={post} key={key} />
+        ))}
+      </Grid>
+    </React.Fragment>
+  )
+}
 
 export { Posts }
