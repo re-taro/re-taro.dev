a199eb0a4c5762e24e9547f673e4e44d1238c190
Re-taro <aminokacho@gmail.com>
2022-05-08 21:28:50 +0900

    🔥 remove spotify files

--- src/utils/fetcher.ts
+++
@@ -1,5 +0,0 @@
-export const fetcher = <Response = unknown>(
-  input: RequestInfo,
-  init?: RequestInit
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-): Promise<Response> => fetch(input, init).then(response => response.json())
