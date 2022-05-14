1f9ae5c3f531e8d9313a8e39b8656536ac1cf2d1
Re-taro <aminokacho@gmail.com>
2022-05-14 12:18:16 +0900

    ✨ fix ogp

--- src/pages/api/ogp.tsx
+++ src/pages/api/ogp.tsx
@@ -1,7 +1,5 @@
-// eslint-disable-next-line unicorn/prefer-node-protocol
-import fs from 'fs'
-// eslint-disable-next-line unicorn/prefer-node-protocol
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import * as chromium from 'playwright-aws-lambda'
 
