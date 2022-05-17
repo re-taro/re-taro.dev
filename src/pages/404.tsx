f0d7a4ca36836c90c07b396c21457100e6e1ef11
Re-taro <aminokacho@gmail.com>
2022-05-18 00:38:44 +0900

    🔍️ improve SEO

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,39 +7,54 @@ import { Button } from "~/components/atoms/button";
 import { Heading } from "~/components/atoms/heading";
 import { Image } from "~/components/atoms/image";
 import { Text } from "~/components/atoms/text";
+import { Seo, OGP_HOST } from "~/components/organisms/seo";
+import type { SeoProperties } from "~/components/organisms/seo";
 
 const ImageBox = tw.div`w-full sm:w-[70%] md:w-[60%] lg:w-[60%] my-0 mx-auto`;
+
+const meta: SeoProperties = {
+  description: "Rintaro Itokawa's Dev Site | re-taro",
+  ogImageUrl: encodeURI(`${OGP_HOST}/api/ogp?title=re-taro`),
+  pageRelPath: "",
+  pagetype: "website",
+  sitename: "re-taro.dev",
+  title: "Rintaro Itokawa - Emotion Seeker",
+  twcardtype: "summary_large_image",
+};
 
 const NotFound: NextPage = () => {
   const { theme } = useTheme();
   const [mounted, setMounted] = useState<boolean>(false);
   useEffect(() => setMounted(true), []);
   return (
-    <div>
-      <ImageBox>
-        <Image
-          src={mounted && theme === "dark" ? "/not_found_dark.svg" : "/not_found_light.svg"}
-          alt={"404 Not found"}
-        />
-      </ImageBox>
-      <Text css={tw`text-center text-sm cursor-pointer hover:underline`}>
-        <a href={"https://undraw.co/illustrations"} target={"_blank"} rel={"noopener noreferrer"}>
-          Illustrations by undraw
-        </a>
-      </Text>
-      <div css={tw`my-4`}>
-        <Heading as={"h3"} css={tw`text-center mb-2`}>
-          Seems like you&apos;re lost.
-        </Heading>
-        <div css={tw`text-center`}>
-          <Link href={"/"} passHref>
-            <Button variant={"normal"} as={"a"}>
-              <Text css={tw`text-lg`}>Let&apos;s Head Back</Text>
-            </Button>
-          </Link>
+    <React.Fragment>
+      <Seo {...meta} />
+      <main>
+        <ImageBox>
+          <Image
+            src={mounted && theme === "dark" ? "/not_found_dark.svg" : "/not_found_light.svg"}
+            alt={"404 Not found"}
+          />
+        </ImageBox>
+        <Text css={tw`text-center text-sm cursor-pointer hover:underline`}>
+          <a href={"https://undraw.co/illustrations"} target={"_blank"} rel={"noopener noreferrer"}>
+            Illustrations by undraw
+          </a>
+        </Text>
+        <div css={tw`my-4`}>
+          <Heading as={"h3"} css={tw`text-center mb-2`}>
+            Seems like you&apos;re lost.
+          </Heading>
+          <div css={tw`text-center`}>
+            <Link href={"/"} passHref>
+              <Button variant={"normal"} as={"a"}>
+                <Text css={tw`text-lg`}>Let&apos;s Head Back</Text>
+              </Button>
+            </Link>
+          </div>
         </div>
-      </div>
-    </div>
+      </main>
+    </React.Fragment>
   );
 };
 
