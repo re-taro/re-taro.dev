17e8d2903c26068c2d69e249910914ce03f5a0af
Rintaro Itokawa <me@re-taro.dev>
2024-01-10 20:03:23 +0900

    feat: use Hono as entrypoint

--- markuplint.config.ts
+++ markuplint.config.ts
@@ -1,20 +1,20 @@
 import type { Config } from "@markuplint/ml-config";
 
 const config: Config = {
-  extends: ["markuplint:recommended"],
-  parser: {
-    "\\.tsx$": "@markuplint/jsx-parser",
-  },
-  specs: {
-    "\\.tsx$": "@markuplint/react-spec",
-  },
-  overrides: {
-    "app/root.tsx": {
-      rules: {
-        "permitted-contents": false,
-      },
-    },
-  },
+	extends: ["markuplint:recommended"],
+	parser: {
+		"\\.tsx$": "@markuplint/jsx-parser",
+	},
+	specs: {
+		"\\.tsx$": "@markuplint/react-spec",
+	},
+	overrides: {
+		"app/root.tsx": {
+			rules: {
+				"permitted-contents": false,
+			},
+		},
+	},
 };
 
 export default config;
