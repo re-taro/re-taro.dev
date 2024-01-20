69be8eb8dc2ff61a8c90cfbe04cfdfa796ac73d1
Rintaro Itokawa <me@re-taro.dev>
2024-01-21 03:40:51 +0900

    feat: add reset css (#485)

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import "./styles/globals.css";
+
 import type { LinksFunction } from "@remix-run/cloudflare";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
