46832dadba062f15d840319f32debefb1733ecb4
Rintaro Itokawa <me@re-taro.dev>
2024-03-25 22:27:51 +0900

    refactor: remove unused weight of font

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ function App(): ReactNode {
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 				<link
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@400..600&family=Noto+Sans+JP:wght@400..600&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Noto+Sans+JP:wght@400;600&display=swap"
 					rel="stylesheet"
 				/>
 				<Meta />
