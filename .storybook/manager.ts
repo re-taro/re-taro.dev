93ea32d0505938eba7cb4d67954d1371889bd04c
Rintaro Itokawa <me@re-taro.dev>
2024-05-30 00:20:01 +0900

    ci(build): deploy storybook workflows

---
+++ .storybook/manager.ts
@@ -0,0 +1,10 @@
+import { addons } from "@storybook/manager-api";
+
+import { create } from "@storybook/theming";
+
+addons.setConfig({
+	theme: create({
+		base: "dark",
+		brandTitle: "re-taro.dev",
+	}),
+});
