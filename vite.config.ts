302db3335bc9e0817d4c6a27ce5a2d1f23c280d7
Rintaro Itokawa <me@re-taro.dev>
2024-11-09 02:10:01 +0900

    build: add v3 features

--- vite.config.ts
+++ vite.config.ts
@@ -57,7 +57,9 @@ export default defineConfig(({ mode }) => ({
 		&& remix({
 			future: {
 				v3_fetcherPersist: true,
+				v3_lazyRouteDiscovery: true,
 				v3_relativeSplatPath: true,
+				v3_singleFetch: true,
 				v3_throwAbortReason: true,
 			},
 			serverModuleFormat: "esm",
